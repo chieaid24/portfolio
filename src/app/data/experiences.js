d461e21e50728b1e23adfb6f7530db3fb7ad389f
Aidan Chien <118775203+chieaid24@users.noreply.github.com>
2026-01-25 09:58:12 -0500

    Update experiences.js

--- src/app/data/experiences.js
+++ src/app/data/experiences.js
@@ -125,7 +125,8 @@ export const experiences = {
           text: (
             <>
               Awards: Systems Design Co-op for Good Award - $12,000,
-              President&apos;s Scholar of Distinction - $2,000
+              President&apos;s Scholar of Distinction - $2,000, President&apos;s
+              Research Award - $1,500
             </>
           ),
         },
