9a1afd427fe390b4db0b489c6f50d6e0ebb4f293
Aidan Chien <118775203+chieaid24@users.noreply.github.com>
2026-04-28 18:33:31 -0700

    update job title

--- src/app/data/experiences.js
+++ src/app/data/experiences.js
@@ -71,7 +71,7 @@ export const experiences = {
         src: "/company-images/autonomous-logo_v2.png",
         alt: "Autonomous Machining logo",
       },
-      title: "Research & Software Development Engineer",
+      title: "R&D Software Engineer",
       subtitle: "Autonomous Machining",
       period: "Dec 2024 - Apr 2025",
       highlights: [
