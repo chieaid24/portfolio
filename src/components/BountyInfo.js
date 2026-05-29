7501006bce37f1783cc0b98ab94f7da5461141df
chieaid24 <aidanmchien@gmail.com>
2026-05-29 17:37:37 -0400

    refine bounty and starflare info popovers

--- src/components/BountyInfo.js
+++ src/components/BountyInfo.js
@@ -20,9 +20,9 @@ export default function BountyInfo() {
           setShowInfo(false);
           setToggleShowInfo(false);
         }}
-        className={`${showInfo || toggleShowInfo ? "text-highlight-color/50" : "text-outline-gray/70"} hover:text-highlight-color/50 p-1 transition duration-150 translate-y-[-1px]`}
+        className={`${showInfo || toggleShowInfo ? "text-highlight-color/50" : "text-outline-gray/70"} hover:text-highlight-color/50 p-1 transition duration-150 translate-y-[0px]`}
       >
-        <Info className="h-3.5 w-3.5" />
+        <Info className="h-3 w-3" />
       </button>
       <AnimatePresence>
         {(showInfo || toggleShowInfo) && (
@@ -31,7 +31,7 @@ export default function BountyInfo() {
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
             transition={{ duration: 0.15 }}
-            className="border-highlight-color/20 bg-background-secondary/60 absolute top-full -left-2 z-20 mt-1 w-48 max-w-[9rem] rounded-xl border-[1px] p-3 text-left text-xs leading-snug text-white shadow-[0_20px_50px_rgba(0,0,0,0.45)] backdrop-blur-lg"
+            className="border-highlight-color/20 bg-background-secondary/80 absolute top-full -left-2 z-20 mt-1 w-48 max-w-[9rem] rounded-xl border-[1px] p-3 text-left text-xs leading-snug text-white shadow-[0_20px_50px_rgba(0,0,0,0.45)] backdrop-blur-lg"
           >
             {`Explore the site to earn galactic credits. Spend them on Themes or Starflares.`}
           </motion.div>
