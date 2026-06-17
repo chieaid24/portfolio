ef5eaa5b0a4d96973b69d3a5a1bd6c5e44686cbb
chieaid24 <aidanmchien@gmail.com>
2026-06-17 17:51:29 -0400

    fix(icons): use currentColor for skill icons

--- src/icons/skills/3D Printing.js
+++ src/icons/skills/3D Printing.js
@@ -1,17 +1,17 @@
 export default function ThreeDPrinting(props) {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="none" {...props}>
-            <path d="M8.33333 3.3333L9.95874 2.52061C9.98466 2.50762 10.0153 2.50762 10.0412 2.52061L11.6667 3.3333" stroke="white" strokeWidth="2.83333" strokeLinecap="round" strokeLinejoin="round" />
-            <path d="M8.33333 9.16663L9.25466 9.62729C9.72383 9.86188 10.2762 9.86188 10.7453 9.62729L11.6667 9.16663" stroke="white" strokeWidth="2.83333" strokeLinecap="round" strokeLinejoin="round" />
-            <path d="M10 10V11.6667" stroke="white" strokeWidth="2.83333" strokeLinecap="round" strokeLinejoin="round" />
-            <path d="M15.4167 5.41663L16.6201 6.21891C16.6492 6.23831 16.6667 6.27097 16.6667 6.30593V7.91663" stroke="white" strokeWidth="2.83333" strokeLinecap="round" strokeLinejoin="round" />
-            <path d="M16.25 6.25L15 7.08333" stroke="white" strokeWidth="2.83333" strokeLinecap="round" strokeLinejoin="round" />
-            <path d="M4.58333 5.41663L3.37539 6.22192C3.34911 6.23943 3.33333 6.26893 3.33333 6.30052V7.91663" stroke="white" strokeWidth="2.83333" strokeLinecap="round" strokeLinejoin="round" />
-            <path d="M3.33333 6.25L4.58333 7.08333" stroke="white" strokeWidth="2.83333" strokeLinecap="round" strokeLinejoin="round" />
-            <path d="M3.33333 12.0834V13.7058C3.33333 13.7335 3.34714 13.7593 3.37014 13.7745L4.58333 14.5834" stroke="white" strokeWidth="2.83333" strokeLinecap="round" strokeLinejoin="round" />
-            <path d="M8.33333 16.6666L9.95966 17.4798C9.98508 17.4925 10.0149 17.4925 10.0403 17.4798L11.6667 16.6666" stroke="white" strokeWidth="2.83333" strokeLinecap="round" strokeLinejoin="round" />
-            <path d="M10 17.5V15.4166" stroke="white" strokeWidth="2.83333" strokeLinecap="round" strokeLinejoin="round" />
-            <path d="M15.4167 14.5834L16.6343 13.7715C16.6546 13.7581 16.6667 13.7355 16.6667 13.7112V12.0834" stroke="white" strokeWidth="2.83333" strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M8.33333 3.3333L9.95874 2.52061C9.98466 2.50762 10.0153 2.50762 10.0412 2.52061L11.6667 3.3333" stroke="currentColor" strokeWidth="2.83333" strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M8.33333 9.16663L9.25466 9.62729C9.72383 9.86188 10.2762 9.86188 10.7453 9.62729L11.6667 9.16663" stroke="currentColor" strokeWidth="2.83333" strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M10 10V11.6667" stroke="currentColor" strokeWidth="2.83333" strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M15.4167 5.41663L16.6201 6.21891C16.6492 6.23831 16.6667 6.27097 16.6667 6.30593V7.91663" stroke="currentColor" strokeWidth="2.83333" strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M16.25 6.25L15 7.08333" stroke="currentColor" strokeWidth="2.83333" strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M4.58333 5.41663L3.37539 6.22192C3.34911 6.23943 3.33333 6.26893 3.33333 6.30052V7.91663" stroke="currentColor" strokeWidth="2.83333" strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M3.33333 6.25L4.58333 7.08333" stroke="currentColor" strokeWidth="2.83333" strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M3.33333 12.0834V13.7058C3.33333 13.7335 3.34714 13.7593 3.37014 13.7745L4.58333 14.5834" stroke="currentColor" strokeWidth="2.83333" strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M8.33333 16.6666L9.95966 17.4798C9.98508 17.4925 10.0149 17.4925 10.0403 17.4798L11.6667 16.6666" stroke="currentColor" strokeWidth="2.83333" strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M10 17.5V15.4166" stroke="currentColor" strokeWidth="2.83333" strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M15.4167 14.5834L16.6343 13.7715C16.6546 13.7581 16.6667 13.7355 16.6667 13.7112V12.0834" stroke="currentColor" strokeWidth="2.83333" strokeLinecap="round" strokeLinejoin="round" />
         </svg>
     )
 }
--- src/icons/skills/Three.js
+++ src/icons/skills/Three.js
@@ -1,4 +1,4 @@
-export default function Tailwind({ color = "#ffffff", ...props }) {
+export default function Tailwind({ color = "currentColor", ...props }) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
