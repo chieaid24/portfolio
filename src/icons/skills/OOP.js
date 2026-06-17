6a3d3843f48f155dc1aaad3ab2ff8029d63313da
chieaid24 <aidanmchien@gmail.com>
2026-06-17 17:51:29 -0400

    fix(icons): use currentColor for skill icons

--- src/icons/skills/OOP.js
+++ src/icons/skills/OOP.js
@@ -10,7 +10,7 @@ export default function OOP(props) {
         fillRule="evenodd"
         clipRule="evenodd"
         d="M1.8 16.2H5.4V12.6H1.8V16.2ZM0 18H7.2V10.8H0V18ZM12.6 5.4H16.2V1.8H12.6V5.4ZM10.8 7.2H18V0H10.8V7.2ZM12.6 9V10.8H13.5V13.5H10.8V12.6H9V16.2H10.8V15.3H15.3V10.8H16.2V9H12.6ZM7.2 5.4V4.5H4.5V7.2H5.4V9H1.8V7.2H2.7V2.7H7.2V1.8H9V5.4H7.2Z"
-        fill="white"
+        fill="currentColor"
       />
     </svg>
   );
