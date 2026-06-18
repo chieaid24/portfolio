5887bc429442a9cb69b47ff8a61b4dc28ed3fb37
chieaid24 <aidanmchien@gmail.com>
2026-06-18 18:03:40 -0400

    feat: add terraform skill icon and canopy skill

---
+++ src/icons/skills/Terraform.js
@@ -0,0 +1,7 @@
+export default function Terraform({ color = "#844FBA", ...props }) {
+  return (
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22 24" fill="none" {...props}>
+  <path d="M0 0V7.575L6.561 11.365V3.787L0 0ZM21.12 4.227L14.559 8.018V15.592L21.119 11.805L21.12 4.227ZM7.28 4.23V11.805L13.841 15.592V8.018L7.28 4.23ZM7.28 12.635V20.21L13.84 24V16.422L7.28 12.635Z" fill={color}/>
+</svg>
+  );
+}
