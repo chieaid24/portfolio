8e297307680c734e2e0a74c9cf129f8f8840885c
chieaid24 <118775203+chieaid24@users.noreply.github.com>
2026-07-15 17:35:26 -0400

    drop next-sitemap, bump eslint-config-next to 16

--- next-sitemap.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://aidanchien.com', // your domain
-  generateRobotsTxt: true,          // creates robots.txt
-  sitemapSize: 7000,
-  changefreq: 'weekly',
-  priority: 0.7,
-  exclude: ['/private-page'],       // paths you don’t want indexed
-};
